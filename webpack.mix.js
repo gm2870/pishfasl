6fe19e0f135fbc80c9e641b016970bfc361c4c1e
Ali <alifarzanegan70@gmail.com>
2020-06-10 16:51:12 +0430

    added slider images

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,3 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.js([
-    'resources/js/app/elements.js',
-    'resources/js/app/index.js'
-    ],
-    'public/js/app.js');
+mix.js(["resources/js/index.js"], "public/js/app.js");
