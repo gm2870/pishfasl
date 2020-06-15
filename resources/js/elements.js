fba02f26c2007d4f4c8898d589902cd85848be36
AliFarzanegan <alifarzanegan70@gmail.com>
2020-06-16 00:17:53 +0430

    added js for mobile menu

---
+++ resources/js/elements.js
@@ -0,0 +1,16 @@
+export const elements = {
+    header_navbar:document.querySelector("#header-navbar"),
+    all_nav_links:document.querySelectorAll('.header-nav-link'),
+    navbar_nav:document.querySelector('.navbar-nav'),
+    header_nav:document.querySelector('header nav'),
+    navbar_toggler:document.querySelector('.navbar-toggler'),
+    overlay:document.querySelector('.overlay'),
+    mobile_menu:document.querySelector('.mobile-menu'),
+    mobile_close_btn:document.querySelector('.mobile-close-btn'),
+    mobile_nav:document.querySelector('.mobile-nav'),
+    mobile_nav_items:document.querySelectorAll('.mobile-nav-item'),
+    portfolio_group_btns:document.querySelectorAll('.filter-group-btn'),
+    filter_group:document.querySelector('.filter-group'),
+    filter_bar_line:document.querySelector('.filter-bar-line'),
+    loader:document.querySelector('.loader')
+};
