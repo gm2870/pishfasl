120f732fdfd25eaee98f64942374ddfb77524cbd
Ali <alifarzanegan70@gmail.com>
2020-06-22 13:52:50 +0430

    added js for scroll event

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,24 +2,21 @@ import $ from "jquery";
 import "bootstrap";
 import "popper.js";
 
-import {elements} from './elements';
+import { elements } from "./elements";
 // stick header after pageYOffset of 60
 
 // window.addEventListener("load", () => {
 //     elements.loader.style.display = 'none';
 // });
 
-const scrollEvent = targetElOffsetTop  => {
-    if(targetElOffsetTop > window.scrollY){
-
+const scrollEvent = targetElOffsetTop => {
+    if (targetElOffsetTop > window.scrollY) {
         // check if header nav is in sticky mode
-        if(elements.header_nav.classList.contains('sticky')){
-            window.scrollBy(0,(targetElOffsetTop - 60 ) - window.scrollY);
-        }
-        else window.scrollBy(0,(targetElOffsetTop - 170) - window.scrollY);
-
-    }else{
-        window.scrollBy(0,(targetElOffsetTop-60) - window.scrollY);
+        if (elements.header_nav.hasClass("sticky")) {
+            window.scrollBy(0, targetElOffsetTop - 60 - window.scrollY);
+        } else window.scrollBy(0, targetElOffsetTop - 60 - window.scrollY);
+    } else {
+        window.scrollBy(0, targetElOffsetTop - 60 - window.scrollY);
     }
 };
 
@@ -27,62 +24,56 @@ const sticky = 60;
 const stickHeader = () => {
     if (window.pageYOffset > sticky) {
         // add class 'sticky' for white background and other styles
-        elements.header_navbar.classList.add("sticky");
-        elements.all_nav_links.forEach(el => {
-            el.classList.add('stickyHeader-link');
-        } );
+        elements.header_navbar.addClass("sticky");
+        elements.all_nav_links.addClass("stickyHeader-link");
     } else {
-        elements.header_navbar.classList.remove("sticky");
-        elements.all_nav_links.forEach(el => {
-            el.classList.remove('stickyHeader-link');
-        } );
+        elements.header_navbar.removeClass("sticky");
+        elements.all_nav_links.removeClass("stickyHeader-link");
     }
 };
 
 const hideMobileMenu = () => {
-    elements.overlay.classList.add('overlay-hidden');
-    elements.mobile_menu.classList.add('mobile-menu-hidden');
-    document.body.style.overflow = 'auto';
+    elements.overlay.addClass("overlay-hidden");
+    elements.mobile_menu.addClass("mobile-menu-hidden");
+    document.body.style.overflow = "auto";
 };
 
 window.addEventListener("scroll", stickHeader);
 
-// handling scroll for click event on navlinks 
-    elements.navbar_nav.addEventListener('click', e => {
-        e.preventDefault();
-        elements.all_nav_links.forEach(el => {
-            el.classList.remove('active')
-        });
-        e.target.classList.add('active');  
-        const target = e.target.innerHTML;
-        let targetElOffsetTop = 0;
-        targetElOffsetTop = document.querySelector(`#${target}`).offsetTop;
-        scrollEvent(targetElOffsetTop);
-    });
+// handling scroll for click event on navlinks
+elements.navbar_nav.on("click", e => {
+    e.preventDefault();
+    const target = $(e.target);
+    elements.all_nav_links.removeClass("active");
+    target.addClass("active");
+    const hrefVal = target.attr("href");
 
-elements.navbar_toggler.addEventListener('click', () => {
-    elements.overlay.classList.remove('overlay-hidden');
-    elements.mobile_menu.classList.remove('mobile-menu-hidden');
-    document.body.style.overflow = 'hidden';
-
+    let targetElOffsetTop = 0;
+    targetElOffsetTop = $(`${hrefVal}`).offset().top;
+    scrollEvent(targetElOffsetTop);
 });
 
-elements.mobile_close_btn.addEventListener('click', () => {
+elements.navbar_toggler.on("click", () => {
+    elements.overlay.removeClass("overlay-hidden");
+    elements.mobile_menu.removeClass("mobile-menu-hidden");
+    document.body.style.overflow = "hidden";
+});
+
+elements.mobile_close_btn.on("click", () => {
     hideMobileMenu();
 });
 
-document.body.addEventListener('click', e => {
-    if( !e.target.closest('.mobile-menu') && e.target.tagName !== 'BUTTON'){
+document.body.addEventListener("click", e => {
+    if (!e.target.closest(".mobile-menu") && e.target.tagName !== "BUTTON") {
         hideMobileMenu();
     }
 });
 
-    elements.mobile_nav.addEventListener('click', e => {
-        hideMobileMenu();
-             
-        const target = e.target.innerHTML;
-        let targetElOffsetTop = 0;
-        targetElOffsetTop = document.querySelector(`#${target}`).offsetTop;
-        scrollEvent(targetElOffsetTop);
-    });
+elements.mobile_nav.on("click", e => {
+    hideMobileMenu();
 
+    const target = $(e.target).html();
+    let targetElOffsetTop = 0;
+    targetElOffsetTop = $(`#${target}`).offset().top;
+    scrollEvent(targetElOffsetTop);
+});
--- resources/js/signup.js
+++ resources/js/signup.js
@@ -1,54 +1,56 @@
-import $ from 'jquery';
-import {elements} from "./elements";
+import $ from "jquery";
+import { elements } from "./elements";
 
-$(document).ready(function () {
-    const navPill = $('.nav-pills li');
-    const nextBtn = $('.btn-next');
-    const prevBtn = $('.btn-previous');
+$(document).ready(function() {
+    const navPill = $(".nav-pills li");
+    const nextBtn = $(".btn-next");
+    const prevBtn = $(".btn-previous");
     let formIsValid = false;
-    $('#advanced-checkbox').on('change',function () {
-        if($('#advanced-checkbox').is(':checked')){
-            $('#advanced').css('display','block');
-        }else {
-            $('#advanced').css('display','none')
+    $("#advanced-checkbox").on("change", function() {
+        if ($("#advanced-checkbox").is(":checked")) {
+            $("#advanced").css("display", "block");
+        } else {
+            $("#advanced").css("display", "none");
         }
     });
-    nextBtn.on('click',function () {
-        prevBtn.prop('disabled',false);
-        const index = $(this).data('step');
-        $(this).data('step',index+1);
-        const current = $(this).data('step') + 1;
+    nextBtn.on("click", function() {
+        prevBtn.prop("disabled", false);
+        const index = $(this).data("step");
+        $(this).data("step", index + 1);
+        const current = $(this).data("step") + 1;
         if (current === navPill.length) {
             $(this).css("display", "none");
             $(".btn-finish").css("display", "block");
-            if(!formIsValid) {
-                $(".btn-finish").prop('disabled',true)
+            if (!formIsValid) {
+                $(".btn-finish").prop("disabled", true);
             }
         }
-        navPill.removeClass('nav-pill-active');
-        $('body .nav-pills li:nth-child(' + current + ')').addClass('nav-pill-active');
-        $('.tab-pane').removeClass('active');
-        $('.tab-pane:nth-child(' + current + ')').addClass('active');
+        navPill.removeClass("nav-pill-active");
+        $("body .nav-pills li:nth-child(" + current + ")").addClass(
+            "nav-pill-active"
+        );
+        $(".tab-pane").removeClass("active");
+        $(".tab-pane:nth-child(" + current + ")").addClass("active");
     });
-    prevBtn.on('click',function () {
+    prevBtn.on("click", function() {
         nextBtn.css("display", "block");
         $(".btn-finish").css("display", "none");
         $(".btn-next").prop("disabled", false);
-        const index = nextBtn.data('step');
-        nextBtn.data('step',index - 1);
-        const current = nextBtn.data('step') + 1;
-        if( current === 1) {
-            prevBtn.prop('disabled',true);
+        const index = nextBtn.data("step");
+        nextBtn.data("step", index - 1);
+        const current = nextBtn.data("step") + 1;
+        if (current === 1) {
+            prevBtn.prop("disabled", true);
         }
-        navPill.removeClass('nav-pill-active');
-        $('body .nav-pills li:nth-child(' + current + ')').addClass('nav-pill-active');
-        $('.tab-pane').removeClass('active');
-        $('.tab-pane:nth-child(' + current + ')').addClass('active');
+        navPill.removeClass("nav-pill-active");
+        $("body .nav-pills li:nth-child(" + current + ")").addClass(
+            "nav-pill-active"
+        );
+        $(".tab-pane").removeClass("active");
+        $(".tab-pane:nth-child(" + current + ")").addClass("active");
     });
-    $(".field-btns input").on("click", function() {
-    });
-    $(".user-type-btn input").on("click", function() {
-    });
+    $(".field-btns input").on("click", function() {});
+    $(".user-type-btn input").on("click", function() {});
     $("#phone").on("blur", function() {
         if (this.value.length < 10 && !$(this).hasClass("input-error")) {
             formIsValid = false;
@@ -56,11 +58,12 @@ $(document).ready(function () {
             $(this).after(
                 "<p class='m-0'><i class='far fa-times-circle red ml-2'></i>شماره تلفن وارد شده صحیح نیست.</p>"
             );
-        }else {
+        } else {
             formIsValid = true;
             $(this).removeClass("input-error");
-            $(this).next().remove();
-
+            $(this)
+                .next()
+                .remove();
         }
     });
     $("#code").on("blur", function() {
@@ -70,39 +73,49 @@ $(document).ready(function () {
             $(this).after(
                 "<p class='error m-0'><i class='far fa-times-circle red ml-2'></i>کد وارد شده صحیح نیست.</p>"
             );
-        }else if(this.value.length === 4) {
+        } else if (this.value.length === 4) {
             formIsValid = true;
             $(this).removeClass("input-error");
-            $(this).next().remove();
+            $(this)
+                .next()
+                .remove();
         }
     });
     $(".user-info input").on("blur", function() {
         if (this.value === "") {
             formIsValid = false;
             $(this).addClass("input-error");
-        }else {
+        } else {
             formIsValid = true;
             $(this).removeClass("input-error");
         }
     });
-    $('.login-form .form-group input').on('blur',function () {
-        if(this.value === '') {
-            $(this).addClass('input-error');
-            $('.login-submit').prop('disabled',true);
-        }else {
+    $("#repeat-password").on("blur", function() {
+        if ($(this).val() !== $("#password").val()) {
+            formIsValid = false;
+            $(this).addClass("input-error");
+        } else {
+            formIsValid = true;
             $(this).removeClass("input-error");
-            $('.login-submit').prop('disabled',false);
-
         }
-    })
-        const carousel1 = $('#carousel1').carousel({
-            interval:false
-        });
-        const carousel2= $('#carousel2').carousel({
-            interval:false
-        });
-        carousel1.on('slide.bs.carousel', function(event) {
-            const to = $(event.relatedTarget).index();
-            carousel2.carousel(to);
-        });
-})
+    });
+    $(".login-form .form-group input").on("blur", function() {
+        if (this.value === "") {
+            $(this).addClass("input-error");
+            $(".login-submit").prop("disabled", true);
+        } else {
+            $(this).removeClass("input-error");
+            $(".login-submit").prop("disabled", false);
+        }
+    });
+    const carousel1 = $("#carousel1").carousel({
+        interval: false
+    });
+    const carousel2 = $("#carousel2").carousel({
+        interval: false
+    });
+    carousel1.on("slide.bs.carousel", function(event) {
+        const to = $(event.relatedTarget).index();
+        carousel2.carousel(to);
+    });
+});
